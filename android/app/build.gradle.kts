9fd31981a6cbd5e4067d68e85af48ce173c04901
Your Name <nofalzia4s@gmail.com>
2025-12-17 00:01:47 +0500

    build: Update Android Gradle configuration to latest standards
    - Updated Kotlin plugin to org.jetbrains.kotlin.android
    - Added multiDexEnabled for Firebase compatibility
    - Cleaned up build configuration comments
    - Updated dependency syntax to Kotlin DSL
    - Set JVM target to string format

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -16,24 +16,20 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.expense_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,15 +40,15 @@ flutter {
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
-    
-    // Import the Firebase BoM
-    implementation platform('com.google.firebase:firebase-bom:32.7.0')
-    
-    // Add the dependencies for Firebase products you want to use
-    implementation 'com.google.firebase:firebase-auth'
-    implementation 'com.google.firebase:firebase-firestore'
-    
-    // Other dependencies
-    implementation 'androidx.multidex:multidex:2.0.1'
+    implementation(kotlin("stdlib-jdk7"))
+
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+    // Firebase products
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    // Multidex
+    implementation("androidx.multidex:multidex:2.0.1")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,32 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+buildscript {
+    val kotlin_version = "1.9.20"
+    repositories {
+        // ADD THESE 3 LINES AT THE TOP:
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        // Other classpaths...
+    }
+}
+
+allprojects {
+    repositories {
+        // ADD THESE 3 LINES HERE TOO:
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        
+        google()
+        mavenCentral()
+    }
+}
